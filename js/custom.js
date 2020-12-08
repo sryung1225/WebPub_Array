ff549b88479f83b56da2457683ec5c392c286fb2
sryung1225 <sryung1225@gmail.com>
2020-12-08 22:58:59 +0900

    banner-slick.js

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+/* banner -swiper.js */
+var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
